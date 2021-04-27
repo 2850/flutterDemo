56ce70a3b581243dd35e3fc8dea7df308043279a
柯盛瑋 <willis.ko@yfyshop.com>
2021-04-27 12:47:56 +0800

    Container、Row、StatelessWidget

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,24 @@ void main() {
 class Homepage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+        appBar: new AppBar(
+          title: Text('MyApp Demo'),
+        ),
+        body: Row(
+          children: [
+            Container(
+              color: Colors.red,
+              width: 100.0,
+              height: 100.0,
+            ),
+            Container(
+              margin: EdgeInsets.all(10.0),
+              color: Colors.red[50],
+              width: 100.0,
+              height: 100.0,
+            )
+          ],
+        ));
   }
 }
