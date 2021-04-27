67a0854c61d055c3d086c51c0157961bef8997de
柯盛瑋 <willis.ko@yfyshop.com>
2021-04-27 12:39:41 +0800

    First Commit

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  return runApp(MaterialApp(
+    home: new Homepage(),
+  ));
+}
+
+class Homepage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
