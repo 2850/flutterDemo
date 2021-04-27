a6c50cd0b4b7f2de362bf2495eaecf2969cbef1c
柯盛瑋 <willis.ko@yfyshop.com>
2021-04-27 13:07:37 +0800

    StatefulWidget

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() {
   return runApp(MaterialApp(
-    home: new Homepage(),
+    home: new Homepage2(),
   ));
 }
 
@@ -28,5 +30,48 @@ class Homepage extends StatelessWidget {
             )
           ],
         ));
+  }
+}
+
+class Homepage2 extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return Homepage2State();
+  }
+}
+
+class Homepage2State extends State<Homepage2> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.forward),
+          onPressed: () {
+            setState(() {});
+          },
+        ),
+        appBar: new AppBar(
+          title: Text('MyApp Demo'),
+        ),
+        body: Row(
+          children: [
+            Container(
+              color: getColor(),
+              width: 100.0,
+              height: 100.0,
+            ),
+            Container(
+              margin: EdgeInsets.all(10.0),
+              color: getColor(),
+              width: 100.0,
+              height: 100.0,
+            )
+          ],
+        ));
+  }
+
+  Color getColor() {
+    return Color.fromARGB((Random().nextInt(255)), Random().nextInt(255),
+        Random().nextInt(255), Random().nextInt(255));
   }
 }
